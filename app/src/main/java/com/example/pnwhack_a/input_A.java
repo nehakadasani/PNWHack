be7a06f410da8dcb9b25c43b41e6e4340fe4ed00
sharais23 <aishisharma29@gmail.com>
2023-02-05 10:23:47 -0800

    aishi

--- app/src/main/java/com/example/pnwhack_a/input_A.java
+++ app/src/main/java/com/example/pnwhack_a/input_A.java
@@ -1,11 +1,42 @@
 package com.example.pnwhack_a;
-import java.util.*;
-public class input_A {
-    private String ingredName;
-    private ArrayList ingredList;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
-    public input_A(String ingredName, int ingredQuantity){
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.*;
+public class input_A extends AppCompatActivity {
+    private String ingredName;
+    private ArrayList<input_A> ingredList;
+    private CheckBox checkBox;
+    private Button recipe;
+    private Button delete;
+    EditText addtolist;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.input);
+
+        checkBox = (checkBox) findViewById(R.id.frizz_A);
+        addtolist = (EditText) findViewById(R.id.textView2_A);
+
+    }
+    public void updateText(View view){
+        checkBox.setText("Hi " + addtolist.getText());
+    }
+
+
+
+
+    /*public input_A(String ingredName){
         this.ingredName = ingredName;
+        this.checkBox = false;
     }
     public String getIngredName(){
         return this.ingredName;
@@ -14,8 +45,9 @@ public class input_A {
     public void setIngredName(String newIngredName){
         this.ingredName = newIngredName;
     }
-    public String toString(){
-        return ;
+    public String getaddtolist(){
+
+        return
     }
     public static void main(String[] args){
         Scanner console = new Scanner(System.in);
@@ -25,8 +57,8 @@ public class input_A {
             int quantity = console.nextInt();
             input_A newIngred = new input_A(name, quantity);
             (this.ingredList).add(newIngred);
-        } while ((console.next()).equals("-1"));
 
-        if ()
+        } while ((console.next()).equals("-1"));
+        */
     }
-}
+
